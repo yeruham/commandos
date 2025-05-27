10d2584c0ac3c92f896d5ce13028903f054f1528
YeruhamMendelson <y0583207549@gmail.com>
2025-05-27 11:09:02 +0300

    new class of sea commando

--- commando/seaCommando.cs
+++ commando/seaCommando.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+public class SeaCommando : Commando
+{
+    public SeaCommando(string name, string codeName) : base(name, codeName) { }
+
+    public void Swimming()
+    {
+        this.status = " swimming";
+        Console.WriteLine($"the commando is {this.status}");
+    }
+}
