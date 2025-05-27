f8553f152b46755f7ae71ac222e22adc670cf048
YeruhamMendelson <y0583207549@gmail.com>
2025-05-27 10:35:26 +0300

    new class of air commando

---
+++ commando/airCommando.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class AirCommando : Commando
+{
+    public AirCommando(string name, string codeName) : base(name, codeName) { }
+
+    public void Parachuting()
+    {
+        this.status = "Parachuting";
+        Console.WriteLine($"the commando is {this.status}");
+    }
+}
