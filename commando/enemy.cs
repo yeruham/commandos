4d4eda9f976cf265047b0d83d9961a26dd38897d
YeruhamMendelson <y0583207549@gmail.com>
2025-05-27 11:45:06 +0300

    class of enemy

--- commando/enemy.cs
+++ commando/enemy.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+public class Enemy
+{
+    string name;
+    int LifeSum = 100;
+    bool isAlive = true;
+
+    public Enemy(string name)
+    {
+        this.name = name;
+    }
+
+    public void Scream()
+    {
+        Console.WriteLine("i am enemy!!!");
+    }
+}
