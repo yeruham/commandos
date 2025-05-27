b82adb23ae7dff20f670815849fd25ab5a9935bf
YeruhamMendelson <y0583207549@gmail.com>
2025-05-27 09:55:52 +0300

    new folder 'commando' with new class on commando soldeir

---
+++ commando/commando.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Commando
+{
+    public string name;
+    public string codeName;
+    public string[] tools = { "Hammer", "chisel", "rope", "bag", "water bottle" };
+    public string status = "standing";
+
+    public Commando(string name, string codeName)
+    {
+        this.name = name;
+        this.codeName = codeName;
+    }
+
+    public void Walk()
+    {
+        this.status = "walk";
+        Console.WriteLine($"the commando {this.name} is {this.status}");
+    }
+
+    public void Hide()
+    {
+        this.status = "hide";
+        Console.WriteLine($"the commando {this.name} is {this.status}");
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine($"the commando {this.name} attack!");
+    }
+}
