6f2f3a25bf510a4a080d88fcfca0fb3f66c6c282
YeruhamMendelson <y0583207549@gmail.com>
2025-05-27 10:03:14 +0300

    new class of weapon

---
+++ commando/weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Weapon
+{
+    string name;
+    string manufacturer;
+    int numBalls;
+
+    public Weapon(string name, string manufacturer, int numBalls)
+    {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.numBalls = numBalls;
+    }
+
+    public void Shoot()
+    {
+        this.numBalls--;
+        Console.WriteLine("a shot was fired.");
+    }
+
+
+}
