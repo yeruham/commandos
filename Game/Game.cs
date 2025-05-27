7809729a1c64b88f4576b4bd90247c1d22da3cdb
YeruhamMendelson <y0583207549@gmail.com>
2025-05-27 15:22:35 +0300

    new class - game - to manage wepons commando's and enemies

---
+++ Game/Game.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+public class Game
+{
+    private WeaponFactory wepones = new WeaponFactory();
+    private CommandoFactory commandoFighters = new CommandoFactory();
+    private EnemyFactory enemies = new EnemyFactory();
+
+    public void CreatAndAddEnemy(string name)
+    {
+        Enemy enemy = enemies.CreatEnemy(name);
+        enemies.AddEnemy(enemy);
+    }
+
+    public void CreatAndAddCommand(string name, string codeName)
+    {
+        Commando commando = commandoFighters.CreatCommando(name, codeName);
+        commandoFighters.AddCommando(commando);
+    }
+
+    public void CreatAndAddWeapon(string name, string manufacturer, int numBalls)
+    {
+        Weapon weapon = wepones.CreatWeapon(name, manufacturer, numBalls);
+        wepones.AddWeapon(weapon);
+    }
+
+    public List<Commando> getcommandoFighters()
+    {
+        return commandoFighters.getCommandoFighters();
+    }
+
+    public List<Enemy> getEnemies()
+    {
+        return enemies.getEnemies();
+    }
+
+    public List<Weapon> getWeapons()
+    {
+        return wepones.getWeaponss();
+    }
+}
